1445a6bce5a460aa1c1a8aac42652d1d48d78450
Abu Ghalib <abughalib64@gmail.com>
2020-10-10 22:51:42 +0530

    database added

--- server/app.js
+++ server/app.js
@@ -1,14 +1,32 @@
 const express = require('express')
 const path = require('path')
+const sqlite = require('sqlite3')
 
 const app = express()
 
 app.set('view engine', 'ejs')
 app.set('views', '../public')
+
 let root = path.join(__dirname, '../')
 let staticDir = path.join(root, 'statics');
-console.log(staticDir)
 app.use('/statics', express.static(staticDir))
+
+let db = new sqlite.Database(path.join(root, 'database/')+'database.db', (err)=>{
+	if(err) throw err.message;
+	console.log("Connected to the database!")
+})
+let table = "users";
+db.all(`select * from ${table}`, [], (err, rows)=>{
+	if(err) throw err;
+	rows.forEach((row)=>{
+		console.log(row.email)
+	})
+})
+
+db.close((err)=>{
+	if(err) throw err;
+	console.log("Server closed!")
+});
 
 app.get('/', (req, res)=>{
 	res.render('index')
@@ -17,5 +35,4 @@ console.log("http://localhost:8080");
 
 app.listen(8080, (err)=>{
 	if(err) console.log(err);
-	console.log("Server listening on Port: ", 8080);
 });

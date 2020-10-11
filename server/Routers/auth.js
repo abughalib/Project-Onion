c40af28fd0ccd25bd22ccf89a50b00ee5fd11590
Abu Ghalib <abughalib64@gmail.com>
2020-10-11 14:16:52 +0530

    auth added.

---
+++ server/Routers/auth.js
@@ -0,0 +1,48 @@
+const hash = require('pbkdf2-password')()
+const session = require('express-session')
+
+app.use(session({
+	resave: false,
+	saveUninitialized: false,
+	secret: "hja&*%&^9jh_lgi*adi3&m3^&o9k23hii**(hi43jkl4()*()&)&%$#%$^&*()__+(&%#@!@!~"
+}))
+
+app.use((req, res, next)=>{
+	let err = req.session.error;
+	let msg = req.session.success;
+	delete req.session.error;
+	delete req.session.success;
+	res.locals.message = '';
+	if(err) res.locals.message = "<p class='msg error'>"+err+"</p>";
+	if(msg) res.locals.message = '<p class="msg success">'+msg+'</p>';
+	next();
+})
+
+function registration(username, password){
+	hash({password: password}, (err, pass, salt, hash)=>{
+		if(err) throw err;
+		// Dummy database for now.
+		let users = {
+			user: {name: 'name'}
+		};
+		users.user.salt = salt;
+		users.user.hash = hash;
+	});
+}
+
+function authenticate(username, password, fun){
+	if(!module.parent) console.log("Authenticating as %s:%s", username, password);
+	//Dummy database for now
+	let users = {
+		user: {name: 'name'}
+	};
+
+	let user = users[username];
+	if(!user) return fun(new Error('User not found!'));
+
+	hash({password: password, salt: user.salt}, (err, password, salt, hash)=>{
+		if(err) throw err;
+		if(hash === user.hash) return fun(null, user)
+		fun (new Error("Invalid password"))
+	})
+}
--- server/Routers/user_router.js
+++ server/Routers/user_router.js
@@ -1,15 +1,24 @@
 const express = require('express');
+const auth = require('./auth')
 
 let router = express.Router();
 
 router.get('/login', (req, res)=>{
-	console.log("Login Page should be here!");
+	res.render('login')
 });
-router.get('/register', (req, res)=>{
-	console.log("Registration Page should be here!");
+router.post('/login', (req, res)=>{
+	let login_info = {
+		username: req.username,
+		password: req.password
+	}
+	console.log(login_info)
+	res.redirect(200, '/user/profile/:id')
 })
-router.get('/profile', (req, res)=>{
-	console.log("Profile Page should be here!");
-});
+router.get('/register', (req, res)=>{
+	res.render('register')
+})
+/*router.get('/profile', (req, res)=>{
+	res.render('profile',)
+});*/
 
 module.exports = router
